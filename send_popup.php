0c471a11d384edb0e988ab13af0c18870249566c
EugenyX_X <38291801+PistrunXvX@users.noreply.github.com>
2021-02-04 14:01:43 +0300

    update

--- send_popup.php
+++ send_popup.php
@@ -1,12 +1,14 @@
 <?php
 
 $name = $_POST['name'];
+$secondName = $_POST['secondName'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 
 $to = 'killerdjek@gmail.com';
 $subject = 'Заявка с сайта Корпоративного Онлайн Университета на отправку модуля';
 $message = "Имя: $name \n
+            Фамилия: $secondName \n
             Телефон: $phone \n
             Email: $email";
 $headers = ("From: $name <$email>" . "\r\n");
